25b14014f7942a5ebfae9d53dd64216d7021af6a
Allen <allen23648858@gmail.com>
2023-11-06 14:49:19 +0800

    11/4

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomerNavBar from './components/CustomerNavBar';
+
+const result = await fetch('http://localhost:8081/users').then((res) => {return res.json();});
+
+function App() {
+  return (
+    <div className="App">
+      <CustomerNavBar/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          { result.msg }
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
