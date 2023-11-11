7c01f91ac121fcd0f878aa7fd07ea77f547bd077
goodyhsu <goodyhsu0510@gmail.com>
2023-11-11 21:38:46 +0800

    Added Customer/Vendor options to login page

--- frontend2/src/pages/Login.tsx
+++ frontend2/src/pages/Login.tsx
@@ -1,7 +1,11 @@
+import {Link} from "react-router-dom";
+
 export default function Login() {
     return (
         <div>
             <h1>Login</h1>
+            <div><Link to="/customer/1"> Customer </Link></div>
+            <div><Link to="/vendor/101"> Vendor </Link></div>
         </div>
     );
 }
