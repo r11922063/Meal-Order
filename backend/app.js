ec0cabdb2f60ad077b111176c5fd772351285cb9
Allen <allen23648858@gmail.com>
2023-11-14 16:37:54 +0800

    Login page ver1.0

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
 import settlementRouter from './routes/Settlement.route.js'
 import allMealsRouter from './routes/allMeals.route.js'
+import loginRouter from './routes/login.route.js'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -37,6 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/allMeals', allMealsRouter);
 app.use('/settlement', settlementRouter);
+app.use('/login', loginRouter);
 
 
 // // catch 404 and forward to error handler
--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -10,25 +10,28 @@ import Vendor from './pages/Vendor';
 import AllMeals from './pages/AllMeals';
 import MealAmount from './pages/MealAmount';
 import VendorLayout from './layouts/VendorLayout';
+import LoginLayout from './layouts/LoginLayout'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 
 const route = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/">
-      <Route index element={<Login/>}/>
-        <Route path="customer/:customerId/" element={<CustomerLayout/>}>
-          <Route index element={<Customer/>}/>
-          <Route path="vendor/:vendorId" element={<OrderMeal/>}/>
-          <Route path="orders" element={<Orders/>}/>
-          <Route path="shopcart" element={<ShopCart/>}/>
-          <Route path="settlement" element={<Settlement identity='customer'/>}/>
-        </Route>
-        <Route path="vendor/:vendorId" element={<VendorLayout/>}>
-          <Route index element={<Vendor/>}/>
-          <Route path="mealamount" element={<MealAmount/>}/>
-          <Route path="allmeals" element={<AllMeals/>}/>
-          <Route path="settlement" element={<Settlement identity='vendor'/>}/>
-        </Route>
+    <Route>
+      <Route path="/" element={<LoginLayout/>}>
+        <Route index element={<Login/>}/>
+      </Route>
+      <Route path="customer/:customerId/" element={<CustomerLayout/>}>
+        <Route index element={<Customer/>}/>
+        <Route path="vendor/:vendorId" element={<OrderMeal/>}/>
+        <Route path="orders" element={<Orders/>}/>
+        <Route path="shopcart" element={<ShopCart/>}/>
+        <Route path="settlement" element={<Settlement identity='customer'/>}/>
+      </Route>
+      <Route path="vendor/:vendorId" element={<VendorLayout/>}>
+        <Route index element={<Vendor/>}/>
+        <Route path="mealamount" element={<MealAmount/>}/>
+        <Route path="allmeals" element={<AllMeals/>}/>
+        <Route path="settlement" element={<Settlement identity='vendor'/>}/>
+      </Route>
     </Route>
   )
 );
