90e0a33991fa9d3d649f4f303c088c1d71365bd3
lyanling <lyling1013@gmail.com>
2023-11-24 17:43:14 +0800

    orders: modify hierarchy(OrderItem & OrderInfoItem)

--- frontend2/src/components/Order/OrderItem.tsx
+++ frontend2/src/components/Order/OrderItem.tsx
@@ -1,5 +1,6 @@
 import type { CustomerOrder, CustomerOrderContent } from '../../type'
 import style from '../../style/Order/OrderItem.module.css'
+import triangle_style from '../../style/Order/TriangleButton.module.css'
 import { BACKEND_URL } from '../../constant'
 import { useState, useEffect } from "react";
 import OrderInfoItem from "./OrderInfoItem";
@@ -9,10 +10,20 @@ export default function OrderItem({ order }: { order: CustomerOrder }) {
     const [order_meals, setOrderMeal] = useState<CustomerOrderContent[]>([]);
     const [disclosure, setDisclosure] = useState(false);
     const [bulk_order, setBulkOrder] = useState(false);
+    const [order_pickup_time, setOrderPickupTime] = useState<Date>(new Date());
+    const [order_cancel_dl, setOrderCancelDL] = useState<Date>(new Date());
 
     const handleDisclosureClick = () => {
         setDisclosure(!disclosure);
     };
+
+    function cancelOrder() {
+        console.log("click cancel button!");
+        // TODO: cancel order
+        // console.log(order_cancel_dl);
+    }
+
+    
 
     useEffect(() => {
         async function fetchOrderMeals() {
@@ -34,19 +45,56 @@ export default function OrderItem({ order }: { order: CustomerOrder }) {
                 console.log("Error fetching all_orders from backend: ", e);
             }
         };
+        
         fetchOrderMeals();
-    }, [order.Meal_List]);
+        setOrderPickupTime(new Date(order.Pickup_Time));
+    }, [order]);
+
+    useEffect(() => {
+        function calOrderCancelDL() {
+            let tmp: Date = order_pickup_time;
+            if (bulk_order === true) {
+                tmp.setHours(order_pickup_time.getHours() - 24);
+            }
+            else {
+                tmp.setHours(order_pickup_time.getHours() - 1);
+            }
+            setOrderCancelDL(tmp);
+        }
+
+        calOrderCancelDL();
+    }, [bulk_order, order_pickup_time]);
+
 
     return (
         <div className={style.orderItem_container}>
-            <div className={style.orderItem_infoItemBox}>
-                <OrderInfoItem
-                    key={order.Order_ID} 
-                    order={order}
-                    handleDisclosureClick={handleDisclosureClick} 
-                    disclosure={disclosure}
-                    bulk_order={bulk_order}
-                />
+            <div className={style.orderItem_infoItemAndButtonContainer}>
+                    <div className={style.orderItem_infoItemContainer}>
+                        <OrderInfoItem
+                            order_id={order.Order_ID}
+                            vendor_name={order.Vendor_Name}
+                            order_pickup_time={order_pickup_time}
+                            order_cancel_dl={order_cancel_dl}
+                        />
+                    </div>
+
+                    <div className={style.orderItem_buttonAndPriceContainer}>
+                        <div className={style.orderItem_cancelBox}>
+                            <button className={style.orderItem_cancelButton} onClick={() => cancelOrder()}>
+                                <span>取消訂單</span>
+                            </button>
+                        </div>
+                        <div className={style.orderItem_totalPriceAndDetail}>
+                            <span>{`總計：${order.Cash_Amount} 元`}</span>
+                            <button className={triangle_style.triangle_buttons} onClick={() => handleDisclosureClick()}>
+                                {disclosure ?
+                                    <div className={`${triangle_style.triangle_buttons__triangle} ${triangle_style.triangle_buttons__triangle_b}`}></div>
+                                    :
+                                    <div className={`${triangle_style.triangle_buttons__triangle} ${triangle_style.triangle_buttons__triangle_l}`}></div>
+                                }
+                            </button>
+                        </div>
+                    </div>
             </div>
             <div>
                 {disclosure ?
