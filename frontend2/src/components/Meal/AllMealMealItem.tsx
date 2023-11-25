f4089052550675c14fc7b7adc763b695bf410fce
goodyhsu <goodyhsu0510@gmail.com>
2023-11-25 19:48:21 +0800

    Refresh after adding new meal

--- frontend2/src/components/Meal/AllMealMealItem.tsx
+++ frontend2/src/components/Meal/AllMealMealItem.tsx
@@ -44,9 +44,8 @@ export default function AllMealMealItem({ meal }: {meal: Meal}) {
         </div>
 
         <div className={style.allMealMealItem_imgBox}>
-          <img src={test_img} className={style.allMealMealItem_img} alt={meal.Meal_Name} />
-          {/* TODO: change to meal.Image_url */}
-          {/* <img src={meal.Image_url} alt={meal.Meal_Name} /> */}
+          <img src={require(`../../assets/meal_imgs/${meal.Image_url}`)} className={style.allMealMealItem_img} alt={meal.Meal_Name} />
+          {/* <img src={require(`../../assets/meal_imgs/10001.png`)} className={style.allMealMealItem_img} alt={meal.Meal_Name} /> */}
         </div>
     </div>
   );
