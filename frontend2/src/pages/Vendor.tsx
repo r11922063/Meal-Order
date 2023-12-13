1e8199cde0f6b11410523e6f142f8c3ff09cde5f
Allen <allen23648858@gmail.com>
2023-12-13 23:50:49 +0800

    add vendor websocket

--- frontend2/src/pages/Vendor.tsx
+++ frontend2/src/pages/Vendor.tsx
@@ -1,11 +1,10 @@
-import { useParams } from "react-router-dom";
+import { useParams, useOutletContext } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { CustomerOrder } from '../type'
-import { BACKEND_URL, WS_URL } from '../constant'
+import { BACKEND_URL } from '../constant'
 import style from '../style/Vendor/Vendor.module.css'
 import VendorOrderTab from "../components/VendorOrder/VendorOrderTab";
-import AutoRefreshComponent from "../components/VendorOrder/AutoRefreshComponent";
-import useWebSocket from "react-use-websocket"
+import { WebSocketHook } from "react-use-websocket/dist/lib/types";
 
 const Today = new Date();
 
@@ -14,7 +13,6 @@ const weekday =['週日', '週一', '週二', '週三', '週四', '週五', '週
 const DayArray = new Array();
 
 for (let step = 0; step < 3; step++){
-    // DayArray.push(day.getFullYear().toString() + "," + (day.getMonth()+1).toString() + "," + day.getDate().toString());
     let day = new Date(Today);
     day.setDate(day.getDate() + step);
     DayArray.push(day);
@@ -26,31 +24,31 @@ export default function Vendor() {
     const [orders, setOrders] = useState<CustomerOrder[]>([]);
     const [selectedDate, setSelectedDate] = useState<string>(((new Date()).getFullYear()).toString() + " 年 " + ((new Date()).getMonth()+1).toString() + " 月 " + (new Date()).getDate().toString() + " 日 " + weekday[(new Date().getDay())]);
     const vendor_id = params.vendorId;
-    //console.log("selectedDate: ", selectedDate);
-
-    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket<CustomerOrder>(WS_URL, {
-        queryParams: { "vendorId": vendor_id! },
-        share: true,
-    })
+    const { sendJsonMessage, lastJsonMessage } = useOutletContext<WebSocketHook<CustomerOrder>>();
     
     useEffect(() => {
         if(lastJsonMessage) {
-            console.log("readyState", readyState);
-            setOrders([...orders, lastJsonMessage]);
+            if (lastJsonMessage.Status === "WAIT_FOR_APPROVAL") {
+                // 新增訂單
+                setOrders([...orders, lastJsonMessage]);
+            }
+            else if (lastJsonMessage.Status === "CANCELLED_UNCHECKED") {
+                // 取消訂單
+                setOrders(orders.map(order => {
+                    return order.Order_ID === lastJsonMessage.Order_ID ? lastJsonMessage : order;
+                }));
+            }
+            else {}
         }
     }, [lastJsonMessage]);
 
     useEffect(() => {
-
         async function fetchOrders(vendorId: string, day: string) {
             try {
-                //console.log("Day: ", day);
-                //console.log("Year: ", year, "Month: ", month, "Date: ", date);
                 const arr = day.split(' ')
                 const year = +arr[0], month = +arr[2], date = +arr[4];
                 const url: string = BACKEND_URL + `/vendor?vendorId=${vendorId}&year=${year}&month=${String(month).padStart(2, '0')}&date=${String(date).padStart(2, '0')}`;
                 const res = await fetch(url).then(res => { return res.json(); });
-                // console.log("[fetechOrders] Result: ", res);
                 setOrders(res);
             } catch (e) {
                 console.log("Error fetching all_orders from backend: ", e);
@@ -61,36 +59,11 @@ export default function Vendor() {
 
     return (
         <div>
-            <h1> 我的訂單 </h1>
-            <button onClick={(e) => {
-                sendJsonMessage({
-                    Order_ID: 101099,
-                    Customer_ID : 1,
-                    Vendor_ID : 101,
-                    Status : 'WAIT_FOR_APPROVAL',
-                    Pickup_Time : '2023-12-12 21:59:59',
-                    Meal_List: [{"Meal_ID": 10001, "Amount": 10}, {"Meal_ID": 10002, "Amount": 25}],
-                    Cash_Amount: 250,
-                    Vendor_Name: "李記水餃1",
-                });
-                sendJsonMessage({
-                    Order_ID: 101199,
-                    Customer_ID : 1,
-                    Vendor_ID : 101,
-                    Status : 'WAIT_FOR_APPROVAL',
-                    Pickup_Time : '2023-12-12 21:59:59',
-                    Meal_List: [{"Meal_ID": 10001, "Amount": 10}, {"Meal_ID": 10002, "Amount": 25}],
-                    Cash_Amount: 250,
-                    Vendor_Name: "李記水餃1",
-                })
-
-                //console.log("送一次訂單");
-            }}>送訂單</button>
+            <h1> 我的訂單 </h1>            
             <select className={style.DropDown} 
                     onChange={e => {
                         let ele = e.target.value;
                         setSelectedDate(((new Date(ele)).getFullYear()).toString() + " 年 " + ((new Date(ele)).getMonth()+1).toString() + " 月 " + (new Date(ele)).getDate().toString() + " 日 " + weekday[(new Date(ele).getDay())]);
-                        //console.log("e.target.value: ", ((new Date(ele)).getFullYear()).toString() + " 年 " + ((new Date(ele)).getMonth()+1).toString() + " 月 " + (new Date(ele)).getDate().toString() + " 日 " + weekday[(new Date(ele).getDay())]);
                     }}> 
                 {DayArray.map(ele => (
                     <option value={ele} key={ele}>
@@ -99,7 +72,6 @@ export default function Vendor() {
                 }
             </select>
             <VendorOrderTab orders={orders}/>
-            {/*<AutoRefreshComponent />*/}
         </div>
     );
 }
